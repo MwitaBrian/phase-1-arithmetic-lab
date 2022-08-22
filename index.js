c0371b0daeafc0765bb9e97ca8935f090cf7f4bd
MwitaBrian <brianmwita13@gmail.com>
2022-08-22 18:53:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Write your code here 
+//multiply
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+//Generate random integer greater than 0
+const random = Math.round(Math.random() * 100) + 1;
+
+//Mod 
+let num3 = 30;
+let num4 = 13;
+const mod = num3 % num4;
+
+//Max value
+const max = Math.max(7, 20, 14, 1, 18);
